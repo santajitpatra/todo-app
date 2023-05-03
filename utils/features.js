75f9fb04b7359b436d6c0a66dc6601da51191b11
SantajitPatra <business.santajitpatra@gmail.com>
2023-05-03 23:16:09 +0530

    hot-fix : takk [id] api

--- utils/features.js
+++ utils/features.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { serialize } from "cookie";
 import jwt from "jsonwebtoken";
+import { User } from "../models/user";
 
 export const connectDB = async () => {
   const { connection } = await mongoose.connect(process.env.MONGO_URL, {
@@ -22,4 +23,15 @@ export const cookieSetter = (res, token, set) => {
 
 export const generateToken = (_id) => {
   return jwt.sign({ _id }, process.env.JWT_SECRET);
+};
+
+export const checkAuth = async (req) => {
+  const cookie = req.headers.cookie;
+  if (!cookie) return null;
+
+  const token = cookie.split("=")[1];
+
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  return await User.findById(decoded._id);
 };
